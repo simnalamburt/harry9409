f93d0580792b87edadd10c68648086b89b22da0b
Hyeon Kim <simnalamburt@gmail.com>
2014-05-01 18:34:26 +0900

    변수 선언위치 조정

--- harry9409.c
+++ harry9409.c
@@ -2,17 +2,16 @@
 #include <string.h>
 
 
-char dic[26000][20]; // 단어 저장(전역변수)
+// dic : 단어 저장
+char dic[26000][20];
 
 int trans(int a, int b) {
-  int eod;
-  int i;
-  int count=0;
+  const int eod = strlen(dic[a]);
 
-  eod = strlen(dic[a]);
-  for(i = 0; i < eod; ++i) {
+  int count=0;
+  for(int i = 0; i < eod; ++i) {
     if(dic[a][i] != dic[b][i]) {
-      count++;
+      ++count;
     }
   }
 
@@ -20,45 +19,39 @@ int trans(int a, int b) {
 }  
 
 int main() {
-  int i, j; //반복문용
-  int num = 0; //문자갯수
-  int ct;
-  int x,y;
-  int len;
-  int max = 0;
+  // num : 문자 갯수
+  int num;
+  
   FILE *fp = fopen("./input.txt", "r");
-
-  for(i = 1; ; ++i) {
+  for(int i = 1; ; ++i) {
     fscanf(fp, " %s ", dic[i]);
     num = i;
-    if(feof(fp)) {
-      break;
-    }
+    if(feof(fp)) break;
   }
-
   fclose(fp);
 
-  for(i = 1; i < num; ++i) {
-    x = i;
-    y = i+1;
-    len = 0;
+  int max = 0;
+  for(int i = 1; i < num; ++i) {
+    int x = i;
+    int y = i + 1;
+    int len = 0;
 
     while(y <= num) {
-      ct = trans(x,y);
+      int ct = trans(x, y);
 
       if(ct == 1) {
         x = y;
-        y = y+1;
+        y = y + 1;
         ++len;
       } else if(ct > 1) {
-        y = y+1;
+        y = y + 1;
       }
     }
 
     if(max <= len) {
-      max=len;
+      max = len;
     }
   }
 
-  printf("%d",max);
+  printf("%d", max);
 }
